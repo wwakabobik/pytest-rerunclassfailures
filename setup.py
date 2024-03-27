2e2125b8391b583bb73d593f4c41baf40950460c
wwakabobik <i.vereshchagin@gmail.com>
2024-03-27 14:19:06 +0100

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(
     name="pytest-rerunclassfailures",
     entry_points={
-        'pytest11': [
-            'pytest_rerunclassfailures = pytest_rerunclassfailures:pytest_rerunclassfailures',
+        "pytest11": [
+            "pytest_rerunclassfailures = pytest_rerunclassfailures:pytest_rerunclassfailures",
         ],
     },
 )
